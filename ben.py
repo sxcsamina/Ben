42f3ef856142d74c7256a39d66853ced2569e7c3
asadofc <mr.asadul.islam00@gmail.com>
2026-08-20 01:17:09 +0600

    Add keep alive server

--- ben.py
+++ ben.py
@@ -1,19 +1,67 @@
-import os
+from os import getenv
 from random import choice
 from dotenv import load_dotenv
-from asyncio import run, sleep
+from aiohttp import ClientSession
+from asyncio import run, sleep, create_task
+from logging import basicConfig, getLogger, INFO
+from aiohttp.web import Application, AppRunner, TCPSite, Response
 from aiogram import F, Bot, Dispatcher
 from aiogram.enums import ChatAction
-from aiogram.types import Message, FSInputFile
 from aiogram.filters import CommandStart
+from aiogram.types import Message, FSInputFile
 
 load_dotenv()
-BOT_TOKEN = os.getenv("BOT_TOKEN") or ""
+basicConfig(format="BEN - %(levelname)s - %(message)s")
+
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+RENDER_URL = getenv("RENDER_EXTERNAL_URL")
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-VOICE_DIR = "voices"
+logger = getLogger(__name__)
+logger.setLevel(INFO)
+
+VOICES = [
+    "voices/hohoho.ogg",
+    "voices/no.ogg",
+    "voices/silly.ogg",
+    "voices/yes.ogg",
+]
+
+async def health(request):
+    return Response(text="Okay", content_type="text/plain")
+
+async def alive():
+
+    if not RENDER_URL:
+        logger.info("Render link not found")
+        return
+
+    while True:
+        await sleep(1 * 60)
+        try:
+            session = ClientSession()
+            await session.get(f"{RENDER_URL}/healthz")
+            logger.debug(f"Pinged {RENDER_URL} to stay alive")
+            await session.close()
+        except Exception as e:
+            logger.error(f"Keep-alive error: {e}")
+
+    logger.info("Up time task is active")
+
+async def poke():
+
+    app = Application()
+    app.router.add_get('/', health)
+    app.router.add_get('/healthz', health)
+    runner = AppRunner(app, access_log=None)
+
+    await runner.setup()
+    site = TCPSite(runner, '0.0.0.0', 8080)
+    await site.start()
+
+    create_task(alive())
 
 @dp.message(CommandStart())
 async def start(message: Message):
@@ -22,33 +70,29 @@ async def start(message: Message):
         await bot.send_chat_action(chat_id=message.chat.id, action=ChatAction.TYPING)
         await message.reply("Hi my name is Ben do you wanna see my dick?")
     except Exception as e:
-        print(f"Error: {e}")
+        logger.error(f"Error: {e}")
 
-@dp.message((F.text.lower().startswith("ben")) | (F.reply_to_message.from_user.id == bot.id))
+@dp.message((F.text.lower().startswith("ben")))
 async def ben(message: Message):
 
     try:
-        voice_files = os.listdir(VOICE_DIR)
-        selected_file = choice(voice_files)
-        file_path = os.path.join(VOICE_DIR, selected_file)
-        voice = FSInputFile(file_path)
-
+        voice = FSInputFile(choice(VOICES))
         await bot.send_chat_action(chat_id=message.chat.id, action=ChatAction.RECORD_VOICE)
         await sleep(1)
-
         await message.reply_voice(voice)
     except Exception as e:
-        print(f"Error: {e}")
+        logger.error(f"Error: {e}")
 
 async def main():
     
-    print("Bot is currently running...")
+    await poke()
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     try:
         run(main())
+        logger.info("Bot is currently running")
     except Exception as e:
-        print(f"Error: {e}")
+        logger.error(f"Bot startup failed: {e}")
     except KeyboardInterrupt:
-        print("Bot properly shut down!")
+        logger.info("Bot properly shut down!")
